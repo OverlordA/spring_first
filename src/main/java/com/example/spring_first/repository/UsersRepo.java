0db9eb4e29bd9c89213e43950e6398bad3c096b2
anton <antonkotik0@gmail.com>
2019-12-19 15:22:05 +0200

    - Added user repository

---
+++ src/main/java/com/example/spring_first/repository/UsersRepo.java
@@ -0,0 +1,14 @@
+package com.example.spring_first.repository;
+
+import com.example.spring_first.models.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+interface UsersRepo extends CrudRepository<UserEntity, Long>{
+
+    List<UserEntity> findAllBy();
+
+}
