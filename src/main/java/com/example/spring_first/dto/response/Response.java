d31111fd63969bf1f38a4c815138d636d70302fe
anton <antonkotik0@gmail.com>
2020-03-12 13:15:07 +0200

    Feat: Added response like base will be contain generic list data User, collection etc.

--- src/main/java/com/example/spring_first/dto/response/Response.java
+++ src/main/java/com/example/spring_first/dto/response/Response.java
@@ -1,4 +1,19 @@
 package com.example.spring_first.dto.response;
 
-public class Response {
+import com.example.spring_first.dto.model.User;
+
+import java.util.List;
+
+public class Response extends BaseResponse {
+
+    private List<User> data;
+
+    public Response(String status, Integer code) {
+        super(status, code);
+    }
+
+    public Response(String status, Integer code, List<User> users ){
+        super(status, code);
+        this.data = users;
+    }
 }
